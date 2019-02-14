85b0f790c9d9fcd6f5c7badc688f92dd3621f08f
Roberto Vecchio <info@robertovecchio.com>
2019-02-14 15:39:06 +0100

    commenti car.h
    - commentato il file car.h

--- Simulatore auto scontri/Simulatore auto scontri/Headers/car.h
+++ Simulatore auto scontri/Simulatore auto scontri/Headers/car.h
@@ -6,22 +6,84 @@
 //  Copyright © 2019 Roberto Vecchio. All rights reserved.
 //
 
-/* - definisco la libreria - */
+                                                                /* - definisco la libreria - */
+                                                            /*-----------------------------------*/
+
 #ifndef car_h
 #define car_h
 
-/* - definizione di costanti - */
+                                                               /* - definizione di costanti - */
+                                                            /*-----------------------------------*/
+
+//definisco la lunghezza massima del nome di un auto
 #define MAX_NAME_LENGHT 20
 
-/* - inclusione di libererie necessarie per la completezza di alcune funzioni - */
+
+
+                                      /* - inclusione di libererie necessarie per la completezza di alcune funzioni - */
+                                 /*---------------------------------------------------------------------------------------*/
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ Includo le librerie standard e non utili per lo sviluppo di alcune funzioni, sono state incluse quindi le librerie :
+ 
+ - stdio.h : "standard input-output header" che contiene definizioni di macro, costanti e dichiarazioni di funzioni e tipi usati per gestire le varie operazioni di
+             input/output. In particolare è stata inclusa allo scopo di utilizzare le funzioni : printf e scanf.
+ 
+ - utilitylib.h : librieria composta da costanti e funzioni di utilità generica, in particolare vengono utilizzate le costanti N_CARS e N_ROWS_COLUMNS in diverse
+                  funzioni
+ 
+ -------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
 #include <stdio.h>
 #include "utilitylib.h"
 
-/* - definizioni di nuovi tipi enum - */
+
+                                                              /* - definizioni di nuovi tipi enum - */
+                                                         /*---------------------------------------------*/
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ Definisco un nuovo tipo enumerativo chiamato direction, che in particolare può assumere 4 valori :
+ 
+ - destra
+ - sinistra
+ - avanti
+ - indietro
+ 
+ -------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 typedef enum { destra = 1, sinistra, avanti, indietro } direction;
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ Qui definisco il tipo booleano, dove false assume il valore di 0 e true assume il valore di 1, utile per gestire alucune logiche del software, infatti, viene anche
+ utilizzato nel main.c
+ 
+ -------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 typedef enum { false, true} bool;
 
-/* - definizioni di nuovi tipi struct - */
+
+
+                                                           /* - definizioni di nuovi tipi struct - */
+                                                       /*---------------------------------------------*/
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ Definisco il tipo car_struct come struttura le cui variabili rappresentano :
+ 
+ {
+ - nome auto [lunghezza massima del nome]
+ - posizione x
+ - posizione y
+ - simbolo di rappresentazione su interfaccia grafica
+ - probabilità di andare a destra in percentuale
+ - probabilità di andare a sinistra in percentuale
+ - probabilità di andare avanti in percentuale
+ - probabilità di andare indietro in percentuale
+ }
+ 
+ -------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
 typedef struct car {
     
     char name[MAX_NAME_LENGHT];
@@ -36,8 +98,14 @@ typedef struct car {
 }car_struct;
 
 
-                                       /* - prototipi di funzioni - */
-                                   /*-----------------------------------*/
+                                                                  /* - prototipi di funzioni - */
+                                                             /*------------------------------------*/
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ Qui definisco i prototipi di funzioni che vengono discussi nel dettaglio nel file source car.c
+ 
+ -------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 
 /* - inizializza la posizione delle auto - */
