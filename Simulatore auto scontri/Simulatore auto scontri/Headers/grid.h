15864be231f05cf4255510fa6158138608b10235
Roberto Vecchio <info@robertovecchio.com>
2019-02-15 13:43:17 +0100

    commenti pt. 3
    - commentato grid.h

--- Simulatore auto scontri/Simulatore auto scontri/Headers/grid.h
+++ Simulatore auto scontri/Simulatore auto scontri/Headers/grid.h
@@ -6,13 +6,74 @@
 //  Copyright © 2019 Roberto Vecchio. All rights reserved.
 //
 
+                                                                /* - Scopo della libreria - */
+                                                           /*-----------------------------------*/
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+                                                                -----------------------------
+                                                        |---->  |           grid.h           |  <----|
+                                                                -----------------------------
+ 
+ La libreria in questione nasce con lo scopo di semplificare le operazioni di creazione di una griglia in stile battaglia navale, infatti la funzione printGrid stamperà
+ a video la seguente interfaccia grafica:
+ 
+ 
+                                                               1   2   3   4   5   6   7   8
+                                                              --- --- --- --- --- --- --- ---
+                                                            1|   |   |   |   |   |   |   |   |1
+                                                              --- --- --- --- --- --- --- ---
+                                                            2|   |   |   |   |   |   |   |   |2
+                                                              --- --- --- --- --- --- --- ---
+                                                            3|   |   |   |   |   |   |   |   |3
+                                                              --- --- --- --- --- --- --- ---
+                                                            4|   |   |   |   |   |   |   |   |4
+                                                              --- --- --- --- --- --- --- ---
+                                                            5|   |   |   |   |   |   |   |   |5
+                                                              --- --- --- --- --- --- --- ---
+                                                            6|   |   |   |   |   |   |   |   |6
+                                                              --- --- --- --- --- --- --- ---
+                                                            7|   |   |   |   |   |   |   |   |7
+                                                              --- --- --- --- --- --- --- ---
+                                                            8|   |   |   |   |   |   |   |   |8
+                                                              --- --- --- --- --- --- --- ---
+                                                               1   2   3   4   5   6   7   8
+ 
+ 
+ -------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+                                                              /* - definisco la libreria - */
+                                                          /*-----------------------------------*/
 #ifndef grid_h
 #define grid_h
 
+
+
+                                        /* - inclusione di libererie necessarie per la completezza di alcune funzioni - */
+                                   /*---------------------------------------------------------------------------------------*/
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ Includo le librerie standard e non, utili per lo sviluppo di alcune funzioni, sono state incluse quindi le librerie :
+ 
+ - stdio.h : "standard input-output header" che contiene definizioni di macro, costanti e dichiarazioni di funzioni e tipi usati per gestire le varie operazioni di
+ input/output. In particolare è stata inclusa allo scopo di utilizzare le funzioni : printf e scanf.
+ 
+ - utilitylib.h : librieria composta da costanti e funzioni di utilità generica, in particolare vengono utilizzate le costanti N_CARS e N_ROWS_COLUMNS in diverse
+ funzioni.
+ 
+ -------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 #include <stdio.h>
 #include "utilitylib.h"
 
-/*stampa intestazione griglia*/
+
+                                                                 /* - prototipi di funzioni - */
+                                                            /*------------------------------------*/
+
+
+/*stampa indice colonna griglia*/
 void printColumnNumbers(void);
 /*stampa solo la griglia centrale*/
 void printPartialGrid(char [N_ROWS_COLUMNS][N_ROWS_COLUMNS]);
